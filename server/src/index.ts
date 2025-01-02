404175c2d1fcd3d4923a8e20a32885d040f149c8
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-01-02 22:43:57 +0530

    Implement basic Express server setup with environment configuration and a root endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import talkToLLM from './utils/talkToLLM';
+
+dotenv.config();
+
+talkToLLM();
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("API is running!!")
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
